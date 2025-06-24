e41a3c576fd2758c7394a61f9399632b95cc0925
jcfebrer <jcfebrer@gmail.com>
2025-06-24 20:16:10 +0200

    Funciones no necesarias

--- FSFormControls/DBGridView/DBGridViewColumn.cs
+++ FSFormControls/DBGridView/DBGridViewColumn.cs
@@ -7,15 +7,5 @@ namespace FSFormControls
 {
     public class DBGridViewColumn : DataGridViewColumn
     {
-        public int MinValue { 
-            get { return this.MinValue; }
-            set { this.MinValue = value; }
-        }
-
-        public int MaxValue
-        {
-            get { return this.MaxValue; }
-            set { this.MaxValue = value; }
-        }
     }
 }
