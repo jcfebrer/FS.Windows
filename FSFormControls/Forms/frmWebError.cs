c55aa3a56ba3cce5e73f8c3d3e910c3d568717fc
jcfebrer <jcfebrer@gmail.com>
2025-11-11 10:38:29 +0100

    Asociar eventos a botones

--- FSFormControls/Forms/frmWebError.cs
+++ FSFormControls/Forms/frmWebError.cs
@@ -57,7 +57,7 @@ namespace FSFormControls
 
                 FSMail.SendMail mail = new FSMail.SendMail(Global.MailServer, Global.MailUserName, Global.MailPassword, Global.MailPort, Global.MailEnableSSL, Global.ProjectName);
 
-                if(mail.SendErrorMail(ErrorMessage))
+                if (mail.SendErrorMail(ErrorMessage))
                     MessageBox.Show("Mensaje enviado.", "Envio correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 else
                     MessageBox.Show("Problemas en el envio del correo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -100,125 +100,126 @@ namespace FSFormControls
         [DebuggerStepThrough]
         private void InitializeComponent()
         {
-            Label1 = new Label();
-            txtName = new TextBox();
-            txtEmail = new TextBox();
-            Label2 = new Label();
-            GroupBox1 = new GroupBox();
-            Label3 = new Label();
-            txtError = new TextBox();
-            dbbSend = new Button();
-            dbbCancel = new Button();
-            GroupBox1.SuspendLayout();
-            SuspendLayout();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.dbbSend = new System.Windows.Forms.Button();
+            this.dbbCancel = new System.Windows.Forms.Button();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // Label1
             // 
-            Label1.AutoSize = true;
-            Label1.Location = new Point(8, 24);
-            Label1.Name = "Label1";
-            Label1.Size = new Size(47, 13);
-            Label1.TabIndex = 0;
-            Label1.Text = "Nombre:";
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(8, 24);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(47, 13);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Nombre:";
             // 
             // txtName
             // 
-            txtName.Anchor = AnchorStyles.Top | AnchorStyles.Left
-                                              | AnchorStyles.Right;
-            txtName.Location = new Point(64, 24);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(250, 20);
-            txtName.TabIndex = 1;
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(64, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(250, 20);
+            this.txtName.TabIndex = 1;
             // 
             // txtEmail
             // 
-            txtEmail.Anchor = AnchorStyles.Top | AnchorStyles.Left
-                                               | AnchorStyles.Right;
-            txtEmail.Location = new Point(64, 56);
-            txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(250, 20);
-            txtEmail.TabIndex = 3;
+            this.txtEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmail.Location = new System.Drawing.Point(64, 56);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(250, 20);
+            this.txtEmail.TabIndex = 3;
             // 
             // Label2
             // 
-            Label2.AutoSize = true;
-            Label2.Location = new Point(8, 56);
-            Label2.Name = "Label2";
-            Label2.Size = new Size(35, 13);
-            Label2.TabIndex = 2;
-            Label2.Text = "Email:";
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(8, 56);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(35, 13);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "Email:";
             // 
             // GroupBox1
             // 
-            GroupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom
-                                                | AnchorStyles.Left
-                                                | AnchorStyles.Right;
-            GroupBox1.Controls.Add(Label3);
-            GroupBox1.Controls.Add(txtError);
-            GroupBox1.Controls.Add(Label2);
-            GroupBox1.Controls.Add(txtName);
-            GroupBox1.Controls.Add(Label1);
-            GroupBox1.Controls.Add(txtEmail);
-            GroupBox1.Location = new Point(16, 8);
-            GroupBox1.Name = "GroupBox1";
-            GroupBox1.Size = new Size(346, 232);
-            GroupBox1.TabIndex = 4;
-            GroupBox1.TabStop = false;
+            this.GroupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GroupBox1.Controls.Add(this.Label3);
+            this.GroupBox1.Controls.Add(this.txtError);
+            this.GroupBox1.Controls.Add(this.Label2);
+            this.GroupBox1.Controls.Add(this.txtName);
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Controls.Add(this.txtEmail);
+            this.GroupBox1.Location = new System.Drawing.Point(16, 8);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(346, 232);
+            this.GroupBox1.TabIndex = 4;
+            this.GroupBox1.TabStop = false;
             // 
             // Label3
             // 
-            Label3.AutoSize = true;
-            Label3.Location = new Point(8, 94);
-            Label3.Name = "Label3";
-            Label3.Size = new Size(196, 13);
-            Label3.TabIndex = 4;
-            Label3.Text = "Información para reproducir el problema:";
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(8, 94);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(196, 13);
+            this.Label3.TabIndex = 4;
+            this.Label3.Text = "Información para reproducir el problema:";
             // 
             // txtError
             // 
-            txtError.Anchor = AnchorStyles.Top | AnchorStyles.Bottom
-                                               | AnchorStyles.Left
-                                               | AnchorStyles.Right;
-            txtError.Location = new Point(8, 112);
-            txtError.Multiline = true;
-            txtError.Name = "txtError";
-            txtError.ScrollBars = ScrollBars.Vertical;
-            txtError.Size = new Size(330, 112);
-            txtError.TabIndex = 5;
+            this.txtError.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtError.Location = new System.Drawing.Point(8, 112);
+            this.txtError.Multiline = true;
+            this.txtError.Name = "txtError";
+            this.txtError.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtError.Size = new System.Drawing.Size(330, 112);
+            this.txtError.TabIndex = 5;
             // 
             // dbbSend
             // 
-            dbbSend.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            dbbSend.Location = new Point(274, 248);
-            dbbSend.Name = "dbbSend";
-            dbbSend.Size = new Size(88, 24);
-            dbbSend.TabIndex = 5;
-            dbbSend.Text = "Enviar";
-            dbbSend.Click += dbbSend_Click;
+            this.dbbSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dbbSend.Location = new System.Drawing.Point(274, 248);
+            this.dbbSend.Name = "dbbSend";
+            this.dbbSend.Size = new System.Drawing.Size(88, 24);
+            this.dbbSend.TabIndex = 5;
+            this.dbbSend.Text = "Enviar";
+            this.dbbSend.Click += new System.EventHandler(this.dbbSend_Click);
             // 
             // dbbCancel
             // 
-            dbbCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            dbbCancel.Location = new Point(178, 248);
-            dbbCancel.Name = "dbbCancel";
-            dbbCancel.Size = new Size(88, 24);
-            dbbCancel.TabIndex = 6;
-            dbbCancel.Text = "Cancelar";
-            dbbCancel.Click += dbbCancel_Click;
+            this.dbbCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dbbCancel.Location = new System.Drawing.Point(178, 248);
+            this.dbbCancel.Name = "dbbCancel";
+            this.dbbCancel.Size = new System.Drawing.Size(88, 24);
+            this.dbbCancel.TabIndex = 6;
+            this.dbbCancel.Text = "Cancelar";
+            this.dbbCancel.Click += new System.EventHandler(this.dbbCancel_Click);
             // 
             // frmWebError
             // 
-            AutoScaleBaseSize = new Size(5, 13);
-            ClientSize = new Size(378, 285);
-            Controls.Add(dbbCancel);
-            Controls.Add(dbbSend);
-            Controls.Add(GroupBox1);
-            Name = "frmWebError";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Formulario Error";
-            GroupBox1.ResumeLayout(false);
-            GroupBox1.PerformLayout();
-            ResumeLayout(false);
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(378, 285);
+            this.Controls.Add(this.dbbCancel);
+            this.Controls.Add(this.dbbSend);
+            this.Controls.Add(this.GroupBox1);
+            this.Name = "frmWebError";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario Error";
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
